1ad874bc88ec1040165c56ce6f0b360cab2100bc
Ian Kettlewell <ian.kettlewell@gmail.com>
2015-02-17 19:57:01 -0500

    Scroll script

---
+++ Impostor/Assets/ScrollScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float speed = 1f;
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			GetComponent<RectTransform>().position -= Time.deltaTime * Vector3.up * speed;
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			GetComponent<RectTransform>().position += Time.deltaTime * Vector3.up * speed;
+		}
+
+		GetComponent<RectTransform>().position += .5f * Vector3.up * speed * Input.GetAxis("Mouse ScrollWheel");
+
+	}
+}
